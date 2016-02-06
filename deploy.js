cd1d758bbefeb59fa3c78ac1441e69b019645955
Niels Gerritsen <n.gerritsen@coolblue.nl>
2016-02-06 14:07:29 +0100

    Added deployment

---
+++ deploy.js
@@ -0,0 +1,18 @@
+var uploadFtp = require('./upload-ftp');
+var argv = require('minimist')(process.argv.slice(2));
+
+uploadFtp({
+  destPath: '/public_html/carloreeser/test/',
+  host: 'carehr.nl',
+  user: argv.user,
+  password: argv.password,
+  dirs: [
+    'css',
+    'files',
+    'img',
+    'js'
+  ],
+  files: [
+    'index.html'
+  ]
+});
