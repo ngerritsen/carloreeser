13daa66a976e4bd3af3628817afacdac07128200
Niels Gerritsen <n.c.gerritsen@gmail.com>
2017-02-12 16:17:54 +0100

    Fix deployment

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ const atImport = require('postcss-import')
 const runSequence = require('run-sequence')
 
 let env = 'production'
-let dest = 'dist'
+let dest = './dist'
 
 gulp.task('css', () => {
   const plugins = [
@@ -23,23 +23,23 @@ gulp.task('css', () => {
     plugins.push(cssnano())
   }
 
-  return gulp.src(['./app/styles/style.css', './app/styles/critical.css'])
+  return gulp.src(['./src/styles/style.css', './src/styles/critical.css'])
     .pipe(postcss(plugins))
     .pipe(gulp.dest(path.join(dest, 'css')))
     .pipe(browserSync.reload({ stream: true }))
 })
 
 gulp.task('files', () => {
-  gulp.src('./app/**/*.{png,pdf}')
+  gulp.src('./src/**/*.{png,pdf}')
     .pipe(gulp.dest(dest))
 })
 
 gulp.task('html', () => {
-  gulp.src('./app/index.ejs')
+  gulp.src('./src/index.ejs')
     .pipe(
         ejs({
           buildNumber: process.env.TRAVIS_BUILD_NUMBER,
-          criticalCssFile: './.tmp/critical.css',
+          criticalCssFile: path.join(dest, 'critical.css')
           fs
         }, null, { ext: '.html' })
           .on('error', gutil.log)
@@ -60,9 +60,9 @@ gulp.task('build', ['css', 'files'], callback => {
 })
 
 gulp.task('watch', () => {
-  gulp.watch('app/styles/**/*.css', ['css'])
-  gulp.watch('./app/**/*.{png,pdf}', ['files'])
-  gulp.watch(['app/index.ejs', path.join(dest, '**/*.css')], ['html'])
+  gulp.watch('src/styles/**/*.css', ['css'])
+  gulp.watch('./src/**/*.{png,pdf}', ['files'])
+  gulp.watch(['src/index.ejs', path.join(dest, '**/*.css')], ['html'])
 })
 
 gulp.task('default', ['css', 'files'], callback => {
